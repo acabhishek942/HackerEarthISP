d76ea5385ff540d3b39d23fbc24601e87eca0771
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-13 01:18:59 +0530

    copied header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,64 @@
 <template>
-  <header>
-    <div class="headline">
-      <h1>{{ title }}</h1>
-      <p> - {{ tagline }}</p>
+  <div>
+    <a href=""><img class="logo"src="/image/download.png"/></a>
+    <p class="heading">ISP HackerEarth</p>
+    <p style="color:#4DB388;font-size:25px;margin-top:50px;float:left;">&nbsp &nbsp&nbsp &nbsp-searching ISP made easy</p>
+    <div class="details"  >Total ISP:09&nbsp &nbsp |&nbsp &nbsp API hits:748</div>
   </div>
-  </header>
-</template>
 
+</template>
 <script>
 export default {
+
   data () {
     return {
-      title: 'ISP HackerEarth',
-      tagline: 'searching ISP made easy!'
+
     }
   },
   methods:{
-  },
-}
+
+    }
+  }
+
 </script>
 
-<style scoped>
-header {
-  padding: 10px;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  text-align: center;
-  display: inline;
+<style >
+.logo {
+    width: 70px;
+    height: 70px;
+    padding:20px;
+
+    float:left;
 }
-h1, p {
-  color: #278e43;
-  text-align: center;
+.heading{
+    margin-left: 20px;
+    font-family: 'Arial';
+    font-size: 50px;
+    font-style: normal;
+    font-variant: normal;
+    color: #4DB388;
+
+    line-height: 26.4px;
+    float:left;
+
 }
-.headline h1, .headline p {
-  display:inline;
+
+.details{
+    background:#4DB388;
+    display: inline-block;
+    font-size:20px;
+    border: 2px solid black;
+    padding-left:25px;
+    padding-right:25px;
+    padding-bottom:5px;
+    padding-top:5px;
+    margin-top:45px;
+    margin-left:200px;
+    border-radius: 10px;
+    text-align:center;
+    width:auto;
+
 }
+
+
 </style>
