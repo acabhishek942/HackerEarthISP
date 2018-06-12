1bfadec0be7fe60e380f582c1ebba769750cd9b2
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-13 00:53:01 +0530

    added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="headline">
+      <h1>{{ title }}</h1>
+      <p> - {{ tagline }}</p>
+  </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'ISP HackerEarth',
+      tagline: 'searching ISP made easy!'
+    }
+  },
+  methods:{
+  },
+}
+</script>
+
+<style scoped>
+header {
+  padding: 10px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  text-align: center;
+  display: inline;
+}
+h1, p {
+  color: #278e43;
+  text-align: center;
+}
+.headline h1, .headline p {
+  display:inline;
+}
+</style>
